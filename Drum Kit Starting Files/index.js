ad5c42760c20bcbdcfbdd1757dd02afef0de1304
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-08-08 12:54:16 +0200

    Add Higher Order Functions folder with exercise about the subject

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,6 +1,6 @@
 let quantidadeBotoes = document.querySelectorAll('.drum').length;
 
-for(let i =0; i < quantidadeBotoes; i++) {
+for(let i = 0; i < quantidadeBotoes; i++) {
     document.querySelectorAll('.drum')[i].addEventListener('click', function() {
         alert('Click!');
     } );
